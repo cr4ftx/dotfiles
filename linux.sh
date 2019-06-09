a9f749f3e06896edd242d36e6488c484ad6f0aef
Swann CASTEL <swann.castel@hotmail.fr>
2019-06-09 20:08:41 +0200

    add vim-plug to install

--- linux.sh
+++ linux.sh
@@ -6,6 +6,9 @@ apt upgrade -y
 apt install zsh
 chsh -s /bin/zsh
 
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 apt install vim
 apt install neovim
 nvim +PlugClean! +qall
