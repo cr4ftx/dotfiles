0790dddb665a7fe0463856427908682d9c7c3363
Swann Castel <swann.castel@hotmail.fr>
2020-10-23 23:56:50 +0200

    refactor: improve install.sh and add colors

--- install.sh
+++ install.sh
@@ -1,56 +1,143 @@
 #!/usr/bin/env bash
 
-if [ ! -d $HOME/.oh-my-zsh ]; then
-    echo exit | sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-    git clone https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/custom/themes/powerlevel10k
-else
-    echo "Oh My Zsh already installed"
-fi
-
-TPM_HOME="$HOME/.tmux/plugins/tpm"
-if [[ ! -d $TPM_HOME ]]; then
-    git clone https://github.com/tmux-plugins/tpm $TPM_HOME
-else
-    echo "TPM already installed in $TPM_HOME"
-fi
-
-if [[ ! -f $HOME/.local/share/nvim/site/autoload/plug.vim ]]; then
-    curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-else
-    echo "Plug Vim is already installed"
-fi
-
-if [[ ! -d $HOME/.cargo ]]; then
-    read -p "Install rustup [yN] ? "
-    if [[ $REPLY =~ ^[yY]$ ]]; then
-        curl https://sh.rustup.rs -sSf | sh
-        . $HOME/.cargo/env
-        rustup component add rustfmt
-        rustup component add clippy
+setup_colors() {
+    # Only use colors if connected to a terminal
+    if [ -t 1 ]; then
+        RED=$(println '\033[31m')
+        GREEN=$(println '\033[32m')
+        YELLOW=$(println '\033[33m')
+        BLUE=$(println '\033[34m')
+        BOLD=$(println '\033[1m')
+        RESET=$(println '\033[m')
+    else
+        RED=""
+        GREEN=""
+        YELLOW=""
+        BLUE=""
+        BOLD=""
+        RESET=""
     fi
-else
-    echo "Rustup is already installed"
-fi
+}
 
-if [[ ! -d $HOME/.nvm ]]; then
-    read -p "Install nvm [yN] ? "
+println() {
+    printf "$@\n"
+}
+
+print_header() {
+    printf $BOLD
+    cat <<-'EOF'
+
+    ____                           _           _       _    __ _ _
+   / ___|_      ____ _ _ __  _ __ ( )___    __| | ___ | |_ / _(_) | ___  ___
+   \___ \ \ /\ / / _` | '_ \| '_ \|// __|  / _` |/ _ \| __| |_| | |/ _ \/ __|
+    ___) \ V  V / (_| | | | | | | | \__ \ | (_| | (_) | |_|  _| | |  __/\__ \
+   |____/ \_/\_/ \__,_|_| |_|_| |_| |___/  \__,_|\___/ \__|_| |_|_|\___||___/
+
+EOF
+    printf $RESET
+}
+
+install_oh_my_zsh() {
+    if [ ! -d ~/.oh-my-zsh ]; then
+        println "${GREEN}\nCloning Oh My Zsh${RESET}"
+        git clone --depth=1 https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
+        println "${GREEN}\nCloning Powerlevel10k${RESET}"
+        git clone https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
+    else
+        println "${BLUE}\nOh My Zsh already installed${RESET}"
+    fi
+}
+
+install_tmux_plugin_manager() {
+    TPM_HOME="$HOME/.tmux/plugins/tpm"
+    if [[ ! -d $TPM_HOME ]]; then
+        println "${GREEN}\nCloning tmux plugins manager${RESET}"
+        git clone https://github.com/tmux-plugins/tpm $TPM_HOME
+    else
+	    println "${BLUE}\nTPM already installed in $TPM_HOME${RESET}"
+    fi
+
+}
+
+install_plug_vim() {
+    if [[ ! -f ~/.local/share/nvim/site/autoload/plug.vim ]]; then
+        println "${GREEN}\nInstalling Plug Vim${RESET}"
+        curl -fLo "${XDG_DATA_HOME:-~/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+           https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    else
+	    println "${BLUE}\nPlug Vim is already installed${RESET}"
+    fi
+}
+
+install_rust_toolchain() {
+    if [[ ! -d ~/.cargo ]]; then
+        echo ""
+        read -p "Install rustup [yN] ? "
         if [[ $REPLY =~ ^[yY]$ ]]; then
-        curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
-        source $HOME/.nvm/nvm.sh
-        nvm install 12
+            println "${GREEN}Installing rust toolchain${RESET}"
+            curl https://sh.rustup.rs -sSf | sh
+            source ~/.cargo/env
+            rustup component add rustfmt
+            rustup component add clippy
+        fi
+    else
+	    println "${BLUE}\nRustup is already installed${RESET}"
     fi
-else
-    echo "NVM is already installed"
-fi
+}
 
-if [[ ! -d $HOME/.deno ]]; then
-    read -p "Install deno [yN] ? "
-    if [[ $REPLY =~ ^[yY]$ ]]; then
-        curl -fsSL https://deno.land/x/install/install.sh | sh
+install_nvm() {
+    if [[ ! -d ~/.nvm ]]; then
+	echo ""
+	read -p "${BOLD}Install nvm [yN] ? ${RESET}"
+	    if [[ $REPLY =~ ^[yY]$ ]]; then
+            println "${GREEN}Installing nvm and node12${RESET}"
+            curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.36.0/install.sh | bash
+            source ~/.nvm/nvm.sh
+            nvm install 12
+        fi
+    else
+	    println "${BLUE}\nNVM is already installed${RESET}"
     fi
-else
-    echo "Deno is already installed"
-fi
+}
 
-nvim +PlugInstall +qa
+install_deno() {
+    if [[ ! -d ~/.deno ]]; then
+        echo ""
+        read -p "Install deno [yN]? "
+        if [[ $REPLY =~ ^[yY]$ ]]; then
+            println "${GREEN}Installing deno${RESET}"
+            curl -fsSL https://deno.land/x/install/install.sh | sh
+        fi
+    else
+	    println "${BLUE}\nDeno is already installed${RESET}"
+    fi
+}
+
+main() {
+    setup_colors
+    print_header
+    install_oh_my_zsh
+    install_tmux_plugin_manager
+    install_plug_vim
+    install_rust_toolchain
+    install_nvm
+    install_deno
+
+    println "${BLUE}\nLinking config files${RESET}"
+    mkdir -p "$HOME/.config/nvim"
+    mkdir -p "$HOME/.local/share/fonts"
+    stow . -v
+
+    if [[ $SHELL != "/bin/zsh" ]]; then
+        println "${YELLOW}\nChanging default shell to zsh...${RESET}"
+        chsh -s /bin/zsh
+    fi
+
+    if [[ ! -d ~/.config/nvim/plugged ]]; then
+        nvim +PlugInstall +qa
+    fi
+
+    println "${GREEN}\nInstallation completed 🚀🔥❤️${RESET}"
+}
+
+main
