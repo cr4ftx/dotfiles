2ef8d13bab906d8af89de99245d7720ad7ab8cf0
Cr4ftx <swann.castel@hotmail.fr>
2025-09-28 14:41:58 +0200

    feat: add nvim-dbee

---
+++ config/.config/nvim/lua/plugins/database.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "kndndrj/nvim-dbee",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+    },
+    build = function()
+      require("dbee").install()
+    end,
+    config = function()
+      require("dbee").setup({})
+    end,
+    cmd = "Dbee",
+  },
+}
